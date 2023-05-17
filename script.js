7d1765b8431d2480556ac6360843dd47d3ea47f7
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-05-17 10:58:05 -0500

    Finalizing data input

--- script.js
+++ script.js
@@ -12,17 +12,6 @@ fileInput.addEventListener("change", () => {
     inputReader.addEventListener("load", () => {
         data = JSON.parse(inputReader.result);
 
-        const iterate = (data) => {
-            Object.keys(data).forEach(key => {
-        
-            console.log(`key: ${key}, value: ${data[key]}`)
-        
-            if (typeof data[key] === 'object' && data[key] !== null) {
-                    iterate(data[key])
-                }
-            })
-        }
-
         parseData(data);
     })
 })
@@ -33,19 +22,34 @@ function parseData(data)
     //Grab tables to write to
     BII_Table = document.getElementById("BII_table");
     Deck_Table = document.getElementById("deck_table");
+    CGR_Table = document.getElementById("CGR_table")
 
-    let defects = data["subFields"][0].subFields
+    let defects = data["subFields"][0].subFields;
+    let inspectors = data.subFields[1].subFields[1].subFields;
 
     //Manual parse for inspection date, bad practice but JSON structure is unpredictable
     document.getElementById("date_BII").textContent = data["subFields"][1]["subFields"][0]["subFields"][0]["name"];
 
+    // Adds all inspectors into the form
+    for (var i in inspectors)
+    {
+        newRow = BII_Table.insertRow(-1);
+        newCell = newRow.insertCell(0);
+        newCell.outerHTML = "<th>INSPECTOR " + i + ":</th>";
+
+        let inspectorName = document.createTextNode(inspectors[i].name);
+        newCell = newRow.insertCell(1);
+        newCell.appendChild(inspectorName);
+    }
+
     for (var i in defects)
     {
-        console.log(defects[i].name);
+        //console.log(defects[i].name);
         newRow = Deck_Table.insertRow(-1);
         
         let text = "NULL"
 
+        // Populates DECK table, each case handles the ith column
         for (let j = 0; j < 6; j++)
         {
             switch(j)
@@ -57,15 +61,31 @@ function parseData(data)
                     text = defects[i].name;
                     break;
                 case 2:
-                    text = "help";
+                    text = defects[i].subFields[0].subFields[0].name;
+                    break;
+                case 3:
+                    text = defects[i].subFields[1].subFields[0].name;
+                    break;
+                case 4:
+                    text = String(defects[i].subFields[2].measurements[0].length);
+                    console.log(defects[i].subFields[2].name);
                     break;
                 default:
                     text = "NULL";
             }
 
             newCell = newRow.insertCell(-1);
-            let newText = document.createTextNode(text);
-            newCell.appendChild(newText);
+            newCell.innerText = text;
+            //let newText = document.createTextNode(text);
+            //newCell.appendChild(newText);
+        }
+
+        // Populates CGR table
+        newRow = CGR_Table.insertRow(-1);
+        for (let i = 0; i < 6; i++)
+        {
+            newCell = newRow.insertCell(-1);
+            newCell.innerText = "NULL";
         }
     }
 }
